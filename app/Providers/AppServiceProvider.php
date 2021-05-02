67320a75363d1f25a1c17fd6c638801f2b4ddb84
rapulu <rapulu@yahoo.com>
2021-05-02 16:02:43 +0100

    fix http scheme

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,7 +26,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Schema::defaultStringLength(191);
-        if(config('app.debug') != true){
+        if(config('app.env') == 'staging'){
             URL::forceScheme('https');
         }
     }
